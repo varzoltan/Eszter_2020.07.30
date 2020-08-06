7a4d0a4b0c516c8132cdff01545c464887f6cee1
varzoltan <64203391+varzoltan@users.noreply.github.com>
2020-08-06 11:17:57 +0200

    11:17

--- Eszter_2020.07.30/Program.cs
+++ Eszter_2020.07.30/Program.cs
@@ -52,7 +52,47 @@ namespace Eszter_2020._07._30
             }
 
             //9. Készíts programot, ami megszámolja egy beolvasott stringben pl. az "a" betűket.
+            Console.Write("Kérem adjon meg egy szövegrészt: ");
+            string szovegresz = Console.ReadLine();
+            int szamlalo = 0; //Összegzés törvénye
+            //char karakter = '=';
+            for (int i = 0;i<szovegresz.Length;i++)
+            {
+                if (szovegresz[i] == 'a')
+                {
+                    szamlalo++;
+                }
+            }
+            Console.WriteLine($"A {szovegresz} - ben az \"a\" betűk száma: {szamlalo}");
 
+            //10. Olvasd be az ax2+bx+c=0 egyenlet együtthatóit, majd írd ki az egyenlet megoldásait!
+            //1.lépés az együtthatók bekérése
+            Console.Write("Kérem adja meg az egyenlet \"a\" együtthatóját! ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Kérem adja meg az egyenlet \"b\" együtthatóját! ");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write("Kérem adja meg az egyenlet \"c\" együtthatóját! ");
+            int c = int.Parse(Console.ReadLine());
+            int reszeredmeny = b  *  b  -  4  *  a  *  c;
+            if (reszeredmeny<0)
+            {
+                Console.WriteLine("Az egyenletnek nincs megoldása");
+            }
+            if (reszeredmeny==0)
+            {
+                double egybeeso = -b/2*a;
+                Console.WriteLine($"Az egyenletnek 2 egybeeső gyöke van melynek értéke: {egybeeso}");
+            }
+            if (reszeredmeny>0)
+            {
+                double egyik = (-b + Math.Sqrt(reszeredmeny)) / 2 * a;
+                double masik = (-b - Math.Sqrt(reszeredmeny)) / 2 * a;
+                Console.WriteLine($"Az eredmények: \nx1={egyik} \nx2={masik}");
+            }
+
+            //11. Írj programot, amely beolvas egy egész számot, majd elosztja 2-vel annyiszor,
+            //ahányszor lehet és közben felírja a számot a kettes számok  
+            //szorzataként megszorozva egy olyan számmal, amely már nem osztható 2-vel. Például: 120=2*2*2*15, 17=17
             Console.ReadKey();
         }
     }
